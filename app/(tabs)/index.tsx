4ffa86bb9cac997bf0bebe955a5177ed9a904b24
nikesh <dhunjunikesh@gmail.com>
2025-01-23 16:14:05 -0500

    Add image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,47 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
 import { Link } from "expo-router"
+import { useState } from "react"
+
+import ImageViewer from "@/components/ImageViewer"
+import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker"
+
+const PlaceHolder = require('@/assets/images/background-image.png')
 
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ['images'],
+        allowsEditing: true,
+        quality: 1
+  })
+
+  if (!result.canceled) {
+    setSelectedImage(result.assets[0].uri)
+  } else {
+    alert("You did not select any image")
+  }
+
+}
+
   return (
-    <View
-      style={styles.container}
-    >
-      <Text
-        style={styles.text}
-      >Home Screen.
+    <View style={styles.container} >
+      <Text style={styles.text}>
+        Home Screen.
       </Text>
       <Link href="/about" style={styles.button}>
         Go to Account Screen
       </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imageSource={PlaceHolder} seletectImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -22,7 +51,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  imageContainer: {
+    flex: 1,
   },
   text: {
     color: '#fff'
@@ -31,5 +63,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
-  }
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
 })
---
+++ components/ImageViewer.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { Image, type ImageSource } from "expo-image";
+
+type Props = {
+    imageSource: ImageSource,
+    seletectImage?: string
+}
+
+export default function ImageViewer({imageSource, seletectImage}: Props) {
+  const finalImageSource = seletectImage ? { uri: seletectImage } : imageSource
+    return <Image source={finalImageSource} style={styles.image} />
+}
+
+const styles = StyleSheet.create({
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
+  });
