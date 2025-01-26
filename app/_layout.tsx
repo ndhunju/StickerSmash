a2faa6417a16b5acb0798ae2073285435de24ee0
nikesh <dhunjunikesh@gmail.com>
2025-01-26 11:29:10 -0500

    Configure status bar, splash screen and app icon.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from 'expo-splash-screen';
+
+// For testing: Show splash screen for at least 5 seconds
+// SplashScreen.preventAutoHideAsync();
+// setTimeout(SplashScreen.hideAsync, 5000);
 
 export default function RootLayout() {
   return (
+    <>
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
+    <StatusBar style="light" />
+    </>
   );
 }
